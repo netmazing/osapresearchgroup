31706d2e7c8661880b91472e3968648d99d72479
Dev <p.trojnacki@o2.pl>
2019-11-09 14:04:04 +0100

    finish projects pages

--- src/pages/en/projects.js
+++ src/pages/en/projects.js
@@ -1,23 +1,57 @@
 import React from 'react';
 import LayoutEn from '../../components/en/LayoutEn';
-// import '../../css/team.css';
-// import SEOen from '../../components/en/SEOen';
-// import TeamEnList from '../../components/team/TeamEnList'
+import '../../css/projects.css';
+import {graphql} from 'gatsby';
+import SEOen from '../../components/en/SEOen';
 
-const projectsEn = () => {
+const projectsEn = ({data}) => {
+    const projectsEn = data.projects.edges;
     return (
         <LayoutEn>
-            {/* <SEOen title="Team" />
-            <main className="team">
-                <header className="team__header header">
-                    <h1 className="team__title title">Our Team</h1>
+            <SEOen title="Research projects" />
+            <main className="projects">
+                <header className="projects__header header">
+                    <h1 className="projects__title title">
+                    Research projects</h1>
                 </header>
+
+                <ul className="projects-list">
+                    {projectsEn.map(({node}) => {
+                        return (
+                            <li className="project-item" key={node.title}>
+                                <h2 className="subsection__title project-item__title">{node.title}</h2>
+                                <p className="project-item__description">{node.description.description}</p>
+                                <p className="project-item__subtitle">Methodology</p>
+                                <p className="project-item__methodology">{node.methodology.methodology}</p>
+                                <p className="project-item__subtitle">Researchers</p>
+                                <p className="project-item__researchers">{node.researchers}</p>
+                            </li>
+                        )
+                        })}            
+                </ul>
+
             </main>
-
-            <TeamEnList/> */}
-
         </LayoutEn>
     )
 }
+
+export const getProjectsEn = graphql`
+query{
+    projects:allContentfulProjects(sort:{fields:date, order:DESC}){
+      edges{
+        node{
+          title
+          description{
+            description
+          }
+          methodology{
+            methodology
+          }
+          researchers
+        }
+      }
+    }
+  }
+`
 
 export default projectsEn
--- src/pages/projekty.js
+++ src/pages/projekty.js
@@ -1,21 +1,56 @@
 import React from 'react';
 import Layout from '../components/Layout';
-// import "../css/team.css";
-// import SEO from '../components/SEO';
-// import TeamList from '../components/team/TeamList'
+import "../css/projects.css";
+import {graphql} from 'gatsby';
+import SEO from '../components/SEO';
 
-const projects = () => {
+const projects = ({data}) => {
+    const projects = data.projekty.edges;
     return (
         <Layout>
-            {/* <SEO title="Zespół" description="Członkowie Zespołu Badawczego Socjologii i Psychologii Organizacji" />
-            <main className="team">
-                <header className="team__header header">
-                    <h1 className="team__title title">Nasz zespół</h1>
+            <SEO title="Projekty badawcze" description="Projekty badawcze, które realizuje zespół socjologii i psychologii organizacji" />
+            <main className="projects">
+                <header className="projects__header header">
+                    <h1 className="projects__title title">Projekty badawcze</h1>
                 </header>
-                <TeamList />
-            </main> */}
+
+                <ul className="projects-list">
+                    {projects.map(({node}) => {
+                        return (
+                            <li className="project-item" key={node.nazwa}>
+                                <h2 className="subsection__title project-item__title">{node.nazwa}</h2>
+                                <p className="project-item__description">{node.opis.opis}</p>
+                                <p className="project-item__subtitle">Metodologia</p>
+                                <p className="project-item__methodology">{node.metodologia.metodologia}</p>
+                                <p className="project-item__subtitle">Badacze</p>
+                                <p className="project-item__researchers">{node.badacze}</p>
+                            </li>
+                        )
+                        })}            
+                </ul>
+
+            </main>
         </Layout>
     )
 }
+
+export const getProjects = graphql`
+query{
+    projekty:allContentfulProjekty(sort:{fields:data, order:DESC}){
+      edges{
+        node{
+          nazwa
+          opis{
+            opis
+          }
+          metodologia{
+            metodologia
+          }
+          badacze
+        }
+      }
+    }
+  }
+`
 
 export default projects
